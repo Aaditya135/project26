be93b2de7d47d1d8f89a4c01b9b87dfe18df8d28
Aaditya135 <66625249+Aaditya135@users.noreply.github.com>
2021-02-13 14:59:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function draw(){
+
+    text("FRENCH FRIES - ")
+    text("1.Peel the potato")
+    text("2.Boil it")
+    text("3.Cut it")
+    text("4.Fry it")
+    text("5.Put salt and peri-peri sauce on it")
+    text("6.Enjoy them and have fun")
+
+    drawSprites()
+    
+}
